89dc1d06770eae4d2f24a11b0cffde33fa4e9053
mikvdev <miguelvargasferreir39@gmail.com>
2026-03-22 13:14:58 -0300

    feat: Adiciona configurações das janelas do jogo

---
+++ main/GamePanel.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class GamePanel extends JFrame {
+    // Configurações da tela
+    // tamanho original de um tile
+    final int originalTileSize = 16; // 16 x 16
+    final int scale = 3; // vamos aumentar o tamnanho
+
+    final int tileSize = originalTileSize * scale;
+    
+
+}
