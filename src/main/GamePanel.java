b4a3a0c42bfa8f82cc1c549190f44de244b23dbf
mikvdev <miguelvargasferreir39@gmail.com>
2026-03-26 12:03:00 -0300

    feat: Adiciona parte do funcionamento do movimento do jogador

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
 
 public class GamePanel extends JPanel implements  Runnable{
 
@@ -18,13 +19,19 @@ public class GamePanel extends JPanel implements  Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; 
 
-
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread; // contador para o fps(frames por segundo)
+
+    int playerX = 100, playerY = 100, playerSpeed = 4;
+
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // Ajuda na renderização (performance)
+        this.addKeyListener(KeyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -46,13 +53,18 @@ public class GamePanel extends JPanel implements  Runnable{
     }
 }
 public void update(){
+    if(keyH.upPressed == true){
+        playerY -= playerSpeed; // vai diminuir do y para subir
+        playerY = playerY - playerSpeed;
+        // min 14:40
+    }
 
 }
 public void paintComponent(Graphics g){ // desenhar objetos na tela, e como um lapis ou pincel
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D)g;
     g2.setColor(Color.white);
-    g2.fillRect(100, 100, tileSize, tileSize);
+    g2.fillRect(playerX,playerY, tileSize, tileSize);
     g2.dispose();
 }
 }
